1b11f15ea46af0a95a071fe5a5ba0b8f316a896d
AlexisDabon <xela619375@gmail.com>
2026-05-25 06:05:00 +0200

    exercise completed

---
+++ fichier_c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+int anneeNaissance = 1994;
+int anneeActuelle = 2026;
+int age = anneeActuelle - anneeNaissance;
+
+printf("en 2026 j'ai %d ans\n", age);
+
+return 0;
+} 
