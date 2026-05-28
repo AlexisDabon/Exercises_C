0aa9b082249266a97d8f76b58fcd8b8fffc0aadd
AlexisDabon <xela619375@gmail.com>
2026-05-28 09:42:28 +0200

    if/else

--- majorite.c
+++ majorite.c
@@ -4,6 +4,12 @@ int main () {
 	int ageUtilisateur;
 	printf(" Quel âge avez vous ? ");
 	scanf("%d" , &ageUtilisateur);
+	if (ageUtilisateur >=18) {
+		printf("vous êtes majeur \n");
+	}
+	else {
+	        printf("vous êtes mineur");
+	}
 		return 0;
 }
 
