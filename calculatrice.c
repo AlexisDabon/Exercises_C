acb96b51ac46c1eeb72793a1e36d13cf4b26aa60
AlexisDabon <xela619375@gmail.com>
2026-05-29 06:44:21 +0200

    printf/scanf

---
+++ calculatrice.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main () { 
+	int nombre1;
+	int nombre2;
+	int choixOperations;
+
+	printf ("Entrez le premier nombre :\n");
+	scanf ("%d", &nombre1);
+
+	printf ("Entrez le second nombre :\n");
+	scanf ("%d", &nombre2);
