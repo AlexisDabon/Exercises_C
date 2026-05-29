68ec2639a87479ac75743ca27bb807d6f8f1a032
AlexisDabon <xela619375@gmail.com>
2026-05-29 10:49:58 +0200

    exercise completed

--- calculatrice.c
+++ calculatrice.c
@@ -10,3 +10,30 @@ int main () {
 
 	printf ("Entrez le second nombre :\n");
 	scanf ("%d", &nombre2);
+
+	printf ("Choisissez l'operation :\n");
+    	printf ("1. Addition (+)\n");
+    	printf ("2. Soustraction (-)\n");
+    	printf ("3. Multiplication (*)\n");
+    	printf ("4. Division (/)\n");
+    	printf ("Votre choix (1-4) : ");
+    	scanf ("%d", &choixOperations);
+
+	if (choixOperations == 1) {
+		printf ("%d", nombre1 + nombre2);
+	}
+	else if (choixOperations == 2) {
+		printf ("%d", nombre1 - nombre2);
+	}
+	else if (choixOperations == 3) { 
+		printf ("%d", nombre1 * nombre2);
+	}
+	else if (choixOperations == 4) { 
+		printf ("%d", nombre1 / nombre2);
+	}
+	else {
+		printf("erreur de saisie \n");
+	}	
+
+	return 0;
+}
