d99f6972df4db96f562ed83a6aa5b7ead7ed3845
AlexisDabon <xela619375@gmail.com>
2026-06-01 12:08:54 +0200

    if/else

--- cinema.c
+++ cinema.c
@@ -6,4 +6,14 @@ int main () {
 
 	float prixBase = 0.0;
 	float prixFinal;
+
+	printf ("Quel âge avez vous?\n");
+	scanf ("%d", &age);
+	
+	if (age <= 0) {
+		printf ("accès refusé , âge non valide \n");
+	}
+	else {
+		printf ("accès autorisé \n");
+	}
 }
