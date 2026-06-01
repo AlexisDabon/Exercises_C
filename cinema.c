3b431b8c4cc6061c29ea923213933254181d3a71
AlexisDabon <xela619375@gmail.com>
2026-06-01 17:53:48 +0200

    menu

--- cinema.c
+++ cinema.c
@@ -17,6 +17,14 @@ int main () {
 	else {
 		printf ("accès autorisé \n");
 	}
+	
+	printf ("salle standard : 9.50 \n");
+	printf ("salle 3D : 12.50 \n");
+	printf ("salle IMAX : 16.00 \n");
+	printf ("votre choix 1-3 \n");
+	scanf ("%d", &choixFilms);
+
+
 
 	return 0;
 }
