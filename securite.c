ab6d513e9d37c1b190a95dbb77df5724fb96fdb2
AlexisDabon <xela619375@gmail.com>
2026-06-04 07:28:30 +0200

    exercise completed

--- securite.c
+++ securite.c
@@ -5,9 +5,12 @@ int main () {
 float note;
 
 do {
-printf ("Taper une note entre 0 et 20 \n");
+printf ("Tapez une note entre 0 et 20 \n");
 scanf ("%f", &note);
 }
 while (note < 0 || note > 20);
 
+printf ("la note est de : %.2f.\n", note); 
+
+return 0;
 }
