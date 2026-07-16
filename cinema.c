ad7d5855c06bda8fee50010e0d975c6b076e1b26
AlexisDabon <xela619375@gmail.com>
2026-07-16 17:03:27 +0200

    feat: array/menu

--- cinema.c
+++ cinema.c
@@ -3,53 +3,21 @@
 int main () {
 	int age;
 	int choixFilms;
+	float prix[] = {9.50, 12.50, 16.00};
 
-	float prixBase = 0.0;
-	float prixFinal;
-
-	printf ("Quel âge avez vous?\n");
-	scanf ("%d", &age);
-	
-	if (age <= 0) {
-		printf ("accès refusé , âge non valide \n");
+	printf ("Quel âge avez vous ? \n");
+	if (scanf("%d", &age) != 1 || age <= 0) {
+		printf ("Accès refusé , âge non valide \n");
 		return 1;
 	}
-	else {
-		printf ("accès autorisé \n");
-	}
-	
-	printf ("salle standard : 9.50 \n");
-	printf ("salle 3D : 12.50 \n");
-	printf ("salle IMAX : 16.00 \n");
-	printf ("votre choix 1-3 \n");
-	scanf ("%d", &choixFilms);
+	printf("Accès autorisé \n");
 
-	switch (choixFilms) {
-		case 1 :
-			prixBase = 9.50;
-			break;
-		case 2 : 
-			prixBase = 12.50;
-			break;
-		case 3 : 
-			prixBase = 16.00;
-			break;
-		default : 
-			printf ("choix invalide \n");
-			return 1;
-	}
+	printf("Salle standard: 9.50 \n, Salle 3D: 12.50 \n, Salle IMAX: 16.00 \n");
+       	if (scanf("%d", &choixFilms) != 1 || choixFilms < 1 || choixFilms > 3) {
+		printf("Choix invalide \n");
+		return 1;
+	}	
 	
-	if (age <= 18) {
-		prixFinal = prixBase*0.80;
-	}
-	else if (age >= 65) {
-		prixFinal = prixBase*0.85;
-	}
-	else {
-		prixFinal = prixBase;
-	}
-	
-	printf ("le prix de votre billet est de : %.2f \n", prixFinal);
 
 	return 0;
 }
