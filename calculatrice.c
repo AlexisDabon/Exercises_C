b9c2b639dcb4cb5f5e963b144a9582cf52edc214
AlexisDabon <xela619375@gmail.com>
2026-07-16 07:56:37 +0200

    feat: interactive menu

--- calculatrice.c
+++ calculatrice.c
@@ -8,14 +8,18 @@ int main (void){
 	int8_t choixOperations;
 
 	printf ("Entrez le premier nombre :\n");
-	if (scanf("%hd", &nombre1) != 1) {
+		if (scanf("%hd", &nombre1) != 1) {
 		return EXIT_FAILURE;
 	}
-
 	printf ("Entrez le second nombre :\n");
-	if (scanf("%hd", &nombre2) != 1) {
+		if (scanf("%hd", &nombre2) != 1) {
 		return EXIT_FAILURE;
 	}
-
+	
+	printf("\n MENU \n 1.addition \n 2.soustraction \n 3.multiplication \n votre choix (1-3)");
+       		if (scanf("%hhd", &choixOperations) != 1) {
+		return EXIT_FAILURE;
+	}
+	
 	return 0;
 }
