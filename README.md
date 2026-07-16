336ecb628068cb2ca1ff42d469321b7282545b63
AlexisDabon <xela619375@gmail.com>
2026-07-16 08:23:29 +0200

    README/style: indentation

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ An optimization of a basic static program that dynamically retrieves the current
 * **Exercise 2**: majorite.c
 Developed an optimized C program using `unsigned int` and strict `scanf` return validation to safely handle age inputs. It robustly prevents crashes and logical errors by verifying input correctness and filtering out biologically impossibleages.
 
-* **Exercise 3** : calculatrice ( calculator / operator ) 
+* **Exercise 3**: Calculator.c
+Memories optimized C calculator that uses precise `stdint.h` types (`int16_t` and `int8_t`) to drastically reduce its binary footprint. It guarantees system-level reliability by strictly enforcing secure input validation and standard `EXIT_FAILURE` error.
+
 * **Exercise 4** : cinema ( float / switch ) 
 * **Exercise 5** : securite ( do / while ) 
 
--- calculatrice.c
+++ calculatrice.c
@@ -17,23 +17,24 @@ int main (void){
 	}
 	
 	printf("\n MENU \n 1.addition \n 2.soustraction \n 3.multiplication \n votre choix (1-3)");
-       		if (scanf("%hhd", &choixOperations) != 1) {
-		return EXIT_FAILURE;
+       	if (scanf("%hhd", &choixOperations) != 1) {
+	return EXIT_FAILURE;
 	}
 	
 	if (choixOperations == 1) {
                 printf("Résultat : %d\n", nombre1 + nombre2);
-        }
+	}
         else if (choixOperations == 2) {
                 printf("Résultat : %d\n", nombre1 - nombre2);
-        }
+	}
         else if (choixOperations == 3) {
                 printf("Résultat : %d\n", nombre1 * nombre2);
-        }
+	}
         else {
-                fprintf(stderr, "Choix invalide \n");
-                return EXIT_FAILURE;
-        }
+       		fprintf(stderr, "Choix invalide \n");
+     		return EXIT_FAILURE;	
+	}
+	
+return EXIT_SUCCESS;
 
-        return EXIT_SUCCESS;
 }
