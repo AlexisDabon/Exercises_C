cad51d7a7d4c0f02aa1bfaff348634d1fc442e78
AlexisDabon <xela619375@gmail.com>
2026-07-16 17:31:12 +0200

    feat: menu/ternary operator

--- cinema.c
+++ cinema.c
@@ -10,14 +10,17 @@ int main () {
 		printf ("Accès refusé , âge non valide \n");
 		return 1;
 	}
+
 	printf("Accès autorisé \n");
 
-	printf("Salle standard: 9.50 \n, Salle 3D: 12.50 \n, Salle IMAX: 16.00 \n");
+	printf("1.Salle standard: 9.50 \n2.Salle 3D: 12.50 \n3.Salle IMAX: 16.00\n");
        	if (scanf("%d", &choixFilms) != 1 || choixFilms < 1 || choixFilms > 3) {
 		printf("Choix invalide \n");
 		return 1;
 	}	
 	
+	float prixFinal = prix[choixFilms - 1] * ((age <= 18) ? 0.80 : (age >= 65) ? 0.85 : 1.00);
+	printf("Le prix de votre billet est de: %.2f \n", prixFinal);
 
 	return 0;
 }
