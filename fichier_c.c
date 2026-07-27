bddf87ce8ecf2d61027b6f7bb1b7e0487b4dd181
AlexisDabon <xela619375@gmail.com>
2026-07-27 03:41:57 +0200

    style: indentation

--- fichier_c.c
+++ fichier_c.c
@@ -3,27 +3,27 @@
 
 int main () {
 
-int anneeNaissance;
-int anneeActuelle;
-int age;
-time_t tempsBrut;
-struct tm *tempsLocal;
+	int anneeNaissance;
+	int anneeActuelle;
+	int age;
+	time_t tempsBrut;
+	struct tm *tempsLocal;
 
-time(&tempsBrut);
-tempsLocal = localtime(&tempsBrut);
-anneeActuelle = tempsLocal->tm_year + 1900;
+	time(&tempsBrut);
+	tempsLocal = localtime(&tempsBrut);
+	anneeActuelle = tempsLocal->tm_year + 1900;
 
-printf("Entrez votre année de naissance : \n");
-scanf("%d", &anneeNaissance);
+	printf("Entrez votre année de naissance : \n");
+	scanf("%d", &anneeNaissance);
 
-if (anneeNaissance > anneeActuelle) {
-	printf("Erreur \n");
-} else {
-	age = anneeActuelle - anneeNaissance;
-	printf("En %d vous avez %d ans \n", anneeActuelle, age);
-}
+	if (anneeNaissance > anneeActuelle) {
+		printf("Erreur \n");
+	} else {
+		age = anneeActuelle - anneeNaissance;
+		printf("En %d vous avez %d ans \n", anneeActuelle, age);
+	}
 
-return 0;
+	return 0;
 
 }
 
